0563eb2d78493ec08ea00c2736b142cdc1c000f5
TheUpstateSound <dave@theupstatesound.com>
2026-05-31 13:27:51 -0500

    fix: separate tsconfig for tests to resolve node16 extension errors

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
 };
